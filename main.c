01a6cab7808edfb4cf90b5b70422feae77b46720
albertx0 <pinheiro.gabriel2020@gmail.com>
2023-11-10 18:28:19 -0300

    Criacao da funcao de filtrar por prioridade.

--- main.c
+++ main.c
@@ -22,15 +22,17 @@ int main() {
             // usuario todas as tarefas
         } else if (opcao == 3) {
             lista_tarefas =
-                    deletarTarefa(lista_tarefas); // Chama a funcao para listar todas as
-            // tarefas ja criadas pelo usuario
-        } else if(opcao == 4) {
+                    deletarTarefa(lista_tarefas); // Chama a funcao para listar todas as tarefas ja criadas pelo usuario
+        }else if(opcao == 6){
+            Filtrar_Tarefas_Prioridade(lista_tarefas);
+        } else if(opcao == 0) {
             escreve(lista_tarefas,"lista"); // Fecha o programa e salva a lista no arquivo
             break;
         }else{
-            printf("Opcao Invalida, por favor digite novamente.\n");
+            printf("Opcao Invalida, por favor digite uma opcao novamente.\n");
         }
     }
 
+    escreve(lista_tarefas , "lista");
     return 0;
 }
